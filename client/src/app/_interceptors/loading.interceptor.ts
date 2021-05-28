3b0cd1cd965e3eef02fd4636c87768cb052a389e
Sandeep <sam63156@hotmail.com>
2021-05-28 14:31:27 -0700

    Pre switch to postgres

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -16,8 +16,7 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
-    return next.handle(request).pipe(
-      delay(100),
+    return next.handle(request).pipe(      
       finalize(() => {
         this.busyService.idle();
       })
