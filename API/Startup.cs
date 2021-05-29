73cb2d69d34c4aa7b22b2cb0c5f09b47082af6f1
Sandeep <sam63156@hotmail.com>
2021-05-29 11:32:46 -0700

    dsfsd bdfgb

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,7 +58,7 @@ namespace API
                            .AllowAnyMethod()
                            .AllowCredentials()
                            .WithExposedHeaders("Pagination")
-                           .WithOrigins("https://localhost:4200"));
+                           .WithOrigins("https://localhost:4200", "https://bettermatch.herokuapp.com"));
 
             app.UseAuthentication();
 
